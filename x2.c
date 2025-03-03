66319c0d80f1ba6a2ece669e9c8ca78792b710fb
Obelix <xxxonlainexxx@mail.ru>
2025-03-03 18:47:31 +0300

    up to v3

--- x2.c
+++ x2.c
@@ -1,44 +1,89 @@
 #include <stdio.h>
 #include <math.h>
+#define MIN -200
+#define MAX 200
 
-void eq(int, int, int);
+int equation_solver (int a, int b, int c, int *root_value, int *root_value2);
+int linear_equation_solver (int b, int c, int *root_value);
 
 int main(){
-	int a, b, c, esc = 1;
-	while (esc){
-		printf("a,b,c долны быть числами\n");
+	int again = 1;
+	while (again){
+		printf("a,b,c долны быть числами от %d до %d\n", MIN, MAX);
 		printf("введите значение a:\n");
+		int a;
 		scanf("%d", &a);
+		if (a < MIN || a > MAX){
+			printf("не верное значение \"a\"\n");
+			continue;
+		}
 		printf("введите значение b:\n");
+		int b;
 		scanf("%d", &b);
+		if (b < MIN || b > MAX){
+			printf ("не верное значение \"b\"\n");
+			continue;
+		}
 		printf("введите значение c:\n");
+		int c;
 		scanf("%d", &c);
-		eq(a,b,c);
-		printf("0 - выход\nдругое число, - продолжить\n");
-		scanf("%d", &esc);
+		if (c < MIN || c > MAX){
+			printf ("не верное значение \"c\"\n");
+			continue;
+		}
+		int root_value, answer;
+		if (a == 0){
+			answer = linear_equation_solver (b, c, &root_value);
+			if (answer == 1)
+				printf("1 корень со значением %d\n", root_value);
+			else if (answer == 2)
+				printf("нет корней\n");
+			else if (answer == 3)
+				printf("корень - любое число\n");
+		}
+		int root_value2;
+		if (a != 0){
+			answer = equation_solver (a, b, c, &root_value, &root_value2);
+			if (answer == 1)
+				printf("нет действительных корней\n");
+			else if (answer == 2)
+				printf("2 корня равных %d\n", root_value);
+			else if (answer == 3)
+				printf("2 различных корня, первый = %d, второй = %d.\n", root_value, root_value2);
+		}
+		while(1){
+			printf("0 - выход\n"
+						 "1 - попробовать снова\n");
+			scanf("%d", &again);
+			if (again != 0 && again != 1)
+				printf("ошибка ввода, попробуйте еще раз\n");
+			else
+				break;
+		}
 	}
 return 0;
 }
 
-void eq(int a, int b, int c){
-	int D, x1, x2;
-	if (a != 0){
-		D = (b*b)-(4*a*c);
-		x1 = ((-b)+(sqrt(D)))/(2*a);
-		x2 = ((-b)-(sqrt(D)))/(2*a);
-		if(D<0)
-			printf("нет действительных корней\n");
-		if(D==0)
-			printf("2 равных корня = %d.\n", x1);
-		if(D>0)
-			printf("2 различных корня, x1 = %d, x2 = %d.\n", x1, x2);
+int linear_equation_solver (int b, int c, int *root_value){
+	if (b != 0){
+		*root_value = -c/-b;
+		return 1;
 	}
-	else if (a == 0 && b != 0){
-		x1 = -c/-b;
-		printf("1 корень, x1 = %d\n", x1);
-	}
-	else if (a == 0 && b == 0 && c != 0)
-		printf("нет корней\n");
-	else if (a == 0 && b == 0 && b == 0)
-		printf("корень - любое число\n");
+	else if (c != 0)
+		return 2;
+	else
+		return 3;
+}
+
+int equation_solver (int a, int b, int c, int *root_value, int *root_value2){
+	int D;
+	D = (b*b)-(4*a*c);
+	*root_value = ((-b) + (sqrt(D))) / (2 * a);
+	*root_value2 = ((-b) - (sqrt(D))) / (2 * a);
+	if(D < 0)
+		return 1;
+	if(D == 0)
+		return 2;
+	if(D > 0)
+		return 3;
 }
