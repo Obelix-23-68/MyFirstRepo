86ef918871cdb3ad375dd27e0d5902ef3d41c617
Obelix <xxxonlainexxx@mail.ru>
2025-04-28 21:18:58 +0300

    v

---
+++ sortstrff.c
@@ -0,0 +1,156 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+#include <ctype.h>
+
+#define INIT_CAP 100
+#define MAX_LINE 100
+
+int fstrcmpfromback(const char *first, const char *second);
+int compare_str_re (const void *a, const void *b);
+int compare_str (const void *a, const void *b);
+int fstrcmp(const char *first, const char *second);
+
+int main(int argc, char *argv[]){
+	setlocale(LC_ALL, "");
+// нахождение кол-ва и содержимого строк, выделение нужной памяти
+	if (argc < 2){
+		printf("so few argc");
+		return 0;
+	}
+	FILE *fsource = fopen(argv[1], "r");
+	if (fsource == NULL){
+		printf("error or opening file");
+		return 0;
+	}
+	char **strings = (char **)malloc(INIT_CAP * sizeof(char*));
+	int cap = INIT_CAP, strcount = 0;
+	char Buf[MAX_LINE];
+	while (fgets(Buf, sizeof(Buf), fsource)){
+		Buf[strcspn(Buf, "\n")] = '\0';
+		if (strcount >= cap){
+			cap *= 2;
+			strings = (char **)realloc(strings, cap * sizeof(char*));
+		}
+		strings[strcount] = (char*)malloc(strlen(Buf) + 1);
+		strcpy(strings[strcount], Buf);
+		strcount++;
+	}
+	strings = (char **)realloc(strings, strcount * sizeof(char*));
+	fclose(fsource);
+//запись в файл оригинала
+	FILE *fdestination = fopen("outfile.txt", "w");
+	for (int i = 0; i < strcount; i++){
+		fputs(strings[i], fdestination);
+		fputc('\n', fdestination);
+	}
+//сортировка с конца
+/*	for (int i = 0; i < strcount - 1;){
+		int value = fstrcmp(strings[i], strings[i + 1]);
+		if (value == 1){
+			char *tmp = strings[i];
+			strings[i] = strings[i + 1];
+			strings[i + 1] = tmp;
+			if (i > 0)
+				i--;
+		}
+		else if (value == -1 || value == 0;)
+			i++;
+	}*/
+	qsort(strings, strcount, sizeof(char*), compare_str_re);
+//запись сортировки наоборот в начало файла
+	rewind(fdestination);
+	for (int i = 0; i < strcount; i++){
+		fputs(strings[i], fdestination);
+		fputc('\n', fdestination);
+	}
+// сортировка с начала
+	qsort(strings, strcount, sizeof(char*), compare_str);
+//запись сортировки с начала в начало файла
+	rewind(fdestination);
+	for(int i = 0; i < strcount; i++){
+		fputs(strings[i], fdestination);
+		fputc('\n', fdestination);
+	}
+	fclose(fdestination);
+	for (int i = 0; i < strcount; i++)
+		free(strings[i]);
+	free(strings);
+	return 0;
+}
+
+int fstrcmpfromback(const char *first, const char *second){
+	int i = 0, j = 0;
+	while(first[i] != '\0')
+		i++;
+	while(second[j] != '\0')
+		j++;
+	while(i >= 0 && j >= 0){
+		if (!isalpha(first[i])){
+			i--;
+			continue;
+		}
+		if (!isalpha(second[j])){
+			j--;
+			continue;
+		}
+		int a = tolower(first[i]);
+		int b = tolower(second[j]);
+		if (a < b)
+			return -1;
+		else if (a > b)
+			return 1;
+		else {
+			i--;
+			j--;
+			continue;
+		}
+	}
+	if (i >= 0)
+		return -1;
+	else if (j >= 0)
+		return 1;
+	return 0;
+}
+
+int compare_str_re (const void *a, const void *b){
+	const char **str1 = (const char **)a;
+	const char **str2 = (const char **)b;
+	return fstrcmpfromback(*str1, *str2);
+}
+int compare_str(const void *a, const void *b){
+	const char **str1 = (const char **)a;
+	const char **str2 = (const char **)b;
+	return fstrcmp(*str1, *str2);
+}
+
+int fstrcmp(const char *first, const char *second){
+	int i = 0, j = 0;
+	while(first[i] != '\0' && second[j] != '\0'){
+		if (!isalpha(first[i])){
+			i++;
+			continue;
+		}
+		if (!isalpha(second[j])){
+			j++;
+			continue;
+		}
+		int a = tolower(first[i]);
+		int b = tolower(second[j]);
+		if (a < b)
+			return -1;
+		else if (a > b)
+			return 1;
+		else {
+			i++;
+			j++;
+			continue;
+		}
+	}
+	if (first[i] == '\0' && second[j] == '\0')
+		return 0;
+	else if (first[i] == '\0')
+		return -1;
+	return 1;
+}
